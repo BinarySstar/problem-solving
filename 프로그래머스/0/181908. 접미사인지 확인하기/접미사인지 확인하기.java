3a4b80613387130481d984348c5b06a0e379fdc8
이진성 <hdss8234@gmail.com>
2024-03-26 09:16:42 +0900

    refactoring: 리팩토링 진행

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,15 +1,11 @@
-import java.util.ArrayList;
-
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        ArrayList<String> suffix = new ArrayList<>();
-        for(int i = 0; i < my_string.length(); i++) {
-            suffix.add(my_string.substring(i));
+        for (int i = 0; i < my_string.length(); i++) {
+            if (my_string.substring(i).equals(is_suffix)) {
+                return 1;
+            }
         }
         
-        for(int i = 0; i < suffix.size(); i++) {
-            if(is_suffix.equals(suffix.get(i))) return 1;
-        }
         return 0;
     }
 }
