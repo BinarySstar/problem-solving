a205a77975deffa734f67252491c6500baded957
이진성 <hdss8234@gmail.com>
2024-03-27 22:10:43 +0900

    [level 0] Title: qr code, Time: 0.06 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.java
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -1,10 +1,9 @@
 class Solution {
     public String solution(int q, int r, String code) {
         StringBuilder sb = new StringBuilder();
-        char [] chr = code.toCharArray();
         for(int i = 0; i < code.length(); i++) {
             if (i % q == r) {
-                sb.append(chr[i]);
+                sb.append(code.charAt(i));
             }
         }
         return sb.toString();
