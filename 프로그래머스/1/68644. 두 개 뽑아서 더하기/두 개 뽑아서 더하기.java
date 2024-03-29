fda10fa0e237627e1ddedea047f212ff16935c7f
이진성 <hdss8234@gmail.com>
2024-03-29 17:21:28 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 11.67 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,12 @@
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        return IntStream.range(0, numbers.length)
+            .flatMap(i -> IntStream.range(i + 1, numbers.length)
+                    .map(j -> numbers[i] + numbers[j]))
+            .distinct()
+            .sorted()
+            .toArray();
+    }
+}
