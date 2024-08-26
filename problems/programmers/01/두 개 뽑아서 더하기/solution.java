fa9136f21ec5a6917178f04eb1f953f4b0354ee0
이진성 <hdss8234@gmail.com>
2024-08-26 14:58:12 +0900

    # 정답 코드

---
+++ problems/programmers/01/두 개 뽑아서 더하기/solution.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < numbers.length - 1; i++) {
+            for(int j = i + 1; j < numbers.length; j++) {
+                list.add(numbers[i] + numbers[j]);
+            }
+        }
+        
+        return list.stream().sorted().distinct().mapToInt(Integer::intValue).toArray();
+    }
+}
